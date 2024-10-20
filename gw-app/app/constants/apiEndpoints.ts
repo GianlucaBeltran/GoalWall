c5aa9dc923ee84fae62f211563d6298317025bc6
GianlucaBeltran <gianlucabeltran@gmail.com>
2024-10-20 16:59:47 +0200

    change apiEndpoint

--- gw-app/app/constants/apiEndpoints.ts
+++ gw-app/app/constants/apiEndpoints.ts
@@ -2,19 +2,11 @@ import { Dispatch } from "react";
 import { AppAction, AppActionType } from "../context/appContext";
 import { User } from "../types/data.types";
 
-const urlSchool = "130.229.135.156"; // gianluca personal ip when connected to KTH VPN
 const urlNgrok = "https://quick-assured-macaque.ngrok-free.app"; // gianluca ngrok, only works when ngrok is running
 
-// This needs to be the ip of the computer that ran the npx expo start command
-const urlHome = process.env.EXPO_PUBLIC_HOME_API; // <--- change this to your local ip address,
-// it is displayed right after the QR code when you run the app:
-// Metro waiting on exp://192.168.0.198:8081 DO NOT USE THE PORT NUMBER (:8081) and exp://
-// Change to:             ^^^^^^^^^^^^^
-// you can also find it by running `ifconfig` on mac
+const urlHome = process.env.EXPO_PUBLIC_HOME_API; // env variable instantied in bash run command
 
 export const url = urlHome || "";
-
-console.log(url, "url");
 
 export async function fetchUser({
   url,
