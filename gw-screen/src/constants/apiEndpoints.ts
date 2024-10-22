d03bf713dd0a4bfe06ec14c21aa98eeffee3434d
GianlucaBeltran <gianlucabeltran@gmail.com>
2024-10-22 11:34:10 +0200

    change screen home url to localhost

--- gw-screen/src/constants/apiEndpoints.ts
+++ gw-screen/src/constants/apiEndpoints.ts
@@ -1,5 +1,5 @@
 const urlSchool = "http://130.229.135.156:3000";
-const urlHome = "http://192.168.0.198:3000";
+const urlHome = "http://127.0.0.1:3000";
 const urlNgrok = "https://quick-assured-macaque.ngrok-free.app:3000";
 
 export const url = urlHome;
